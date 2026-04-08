8a5dc18a6e50cc3fe98d66458ddaf2fcd42837b9
Simona127 <grachkasimona@gmail.com>
2026-04-08 17:16:18 +0300

    Add ReviewController and review creation functionality

---
+++ BookSwapLite/Controllers/ReviewController.cs
@@ -0,0 +1,29 @@
+﻿using BookSwapLite.Services.Reviews;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookSwapLite.Controllers
+{
+    [Authorize]
+    public class ReviewController : Controller
+    {
+        private readonly IReviewService reviewService;
+        public ReviewController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+        public IActionResult Add(string userId)
+        {
+            ViewBag.UserId = userId;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(string userId, int rating, string? comment)
+        {
+            string reviewerId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await reviewService.AddReviewAsync(reviewerId!, userId, rating, comment);
+            return RedirectToAction("Profile", "User", new { id = userId });
+        }
+    }
+}
